c5d61bc32f5b66ea1a60e8f6924b444d4fbd2465
MalinaD <malina.demirova@gmail.com>
2014-09-05 15:46:54 +0300

    add elements to iAmOn function

--- Actions/Footer/CustomerService.php
+++ Actions/Footer/CustomerService.php
@@ -1,23 +1,23 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . '/General.php');
+require_once(dirname(dirname(__FILE__)) . '/' . '../General.php');//change dir
 
 class FooterActionsCustomerService extends GeneralActions
 {
     public function goToMyAccount()
     {
         $this->byCssSelector('.st_footer_customerService_myAccount')->click();
-        $this->iAmOn('LoginOrRegister');
+        $this->iAmOn('LoginOrRegister',null,1);//add element
     }
 
     public function goToReturns()
     {
         $this->byCssSelector('.st_footer_customerService_returns')->click();
-        $this->iAmOn('Returns');
+        $this->iAmOn('Returns',null,1);
     }
 
     public function goToContactPage()
     {
         $this->byCssSelector('.st_footer_customerService_contacts')->click();
-        $this->iAmOn('Contact');
+        $this->iAmOn('Contact',null,1);
     }
 }
