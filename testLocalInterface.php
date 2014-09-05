0c1e6e5801f764030bd2079980da6e15a4d1c03c
MalinaD <malina.demirova@gmail.com>
2014-09-05 14:54:23 +0300

    change dir, remove unused variable , add value to countryCode within

--- testLocalInterface.php
+++ testLocalInterface.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once(dirname(__FILE__) . '/Scenarios/General.php');
+require_once(dirname(__FILE__) . '../Scenarios/General.php');//dir change
 class LocalInterface extends PHPUnit_Extensions_Selenium2TestCase
 {
     public $countryCode;
 
-	protected function setUp() {
-        global $argv, $argc;
+    protected function setUp() {
+        global $argv; //remove $argc, it is not used
         $this->countryCode = $argv[2];
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://google.com/');
     }
 
     public function testHeader() {
-        $session = $this->prepareSession();
+        $this->$session = prepareSession();//change $session
         $this->currentWindow()->maximize();
         $headerScenario = new GeneralScenario;
         $headerScenario->setI($this);
-        $headerScenario->setTranslation($this->countryCode);
+        $headerScenario->setTranslation($this->countryCode= $argv[2]);//add argument
         $headerScenario->runGeneralScenario();
     }
 }
