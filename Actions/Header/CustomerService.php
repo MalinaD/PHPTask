2ed140d7d78884e0872c9408027b97f1515c9ca1
MalinaD <malina.demirova@gmail.com>
2014-09-05 15:48:02 +0300

    add elements to iAmOn function

--- Actions/Header/CustomerService.php
+++ Actions/Header/CustomerService.php
@@ -7,31 +7,31 @@ class HeaderActionsCustomerService extends GeneralActions {
         $this->hoverOnCustomerService();
         //$this->byCssSelector("li.service-menu-item:nth-child(3) > div:nth-child(3) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)")->click();
         $this->byCssSelector(".st_header_customerService_myAccount")->click();
-        $this->iAmOn('LoginOrRegister');
+        $this->iAmOn('LoginOrRegister',null,1);
     }
 
     public function goToSizeTables() {
         $this->hoverOnCustomerService();
         $this->byLinkText($this->translation['Size tables'])->click();
-        $this->iAmOn('SizeTables');
+        $this->iAmOn('SizeTables',null,1);
     }
 
     public function goToReturns() {
         $this->hoverOnCustomerService();
         $this->byLinkText($this->translation['Returns'])->click();
-        $this->iAmOn('Returns');
+        $this->iAmOn('Returns',null,1);//add elements
     }
 
     public function goToFAQ() {
         $this->hoverOnCustomerService();
         $this->byLinkText($this->translation['FAQ'])->click();
-        $this->iAmOn('FAQ');
+        $this->iAmOn('FAQ',null,1);
     }
 
     public function goToContactPage() {
         $this->hoverOnCustomerService();
         $this->byLinkText($this->translation['Contact'])->click();
-        $this->iAmOn('Contact');
+        $this->iAmOn('Contact',null,1);
     }
 }
 ?>
